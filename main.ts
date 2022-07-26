af18fe67d6936694d7f08231b8d6718dd148c0d9
Hacker-C <1507559148@qq.com>
2022-07-26 11:01:00 +0800

    🎉 feat(11-next-dom-update): Finish #11

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './questions/10-lifecycle/App.vue'
+import App from './questions/11-next-dom-update/App.vue'
 
 createApp(App).mount('#app')
---
+++ questions/11-next-dom-update/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, nextTick } from 'vue'
+const count = ref(0)
+const counter = ref(null)
+async function increment() {
+  count.value++
+  /**
+   * DOM is not yet updated, how can we make sure that the DOM gets updated
+   * Make the output be true
+   */
+  // make sure the we can get the properly DOM, use nextTick()
+  // 确保能正确获取更新之后的 DOM，使用 nextTick()
+  // Vue docs: https://vuejs.org/api/general.html#nexttick
+  await nextTick()
+  console.log(+counter.value.textContent === 1)
+}
+</script>
+
+<template>
+  <button ref="counter" @click="increment">
+    {{ count }}
+  </button>
+</template>
