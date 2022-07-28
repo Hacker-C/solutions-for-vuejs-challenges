c1526521b479d0058bc827ee8252dcf87887b576
Hacker-C <1507559148@qq.com>
2022-07-28 13:56:32 +0800

    🎉 feat(9-dependency-injection): Finish #9

---
+++ questions/9-dependency-injection/Child.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+// Add a piece of code to make the `count` value get injected into the child component.
+// provide/inject
+import { inject } from 'vue'
+
+const count = inject('count')
+</script>
+
+<template>
+  {{ count }}
+</template>
