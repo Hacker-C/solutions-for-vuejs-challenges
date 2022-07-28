4a43f46abdf84d28818f2d99c249918e1c7b15dc
Hacker-C <1507559148@qq.com>
2022-07-28 15:30:54 +0800

    🎉 feat: Finish #14

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './questions/13-dom-portal/App.vue'
+import App from './questions/14-dynamic-css-values/App.vue'
 
 createApp(App).mount('#app')
---
+++ questions/14-dynamic-css-values/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+
+import AppRaw from './App.vue?raw'
+
+describe('DomPortal', () => {
+  it('render to body', () => {
+    expect(AppRaw).toContain(atob('Y29sb3I6IHYtYmluZCh0aGVtZSk='))
+  })
+})
