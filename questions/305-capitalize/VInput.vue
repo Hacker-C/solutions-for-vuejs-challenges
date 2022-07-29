7d57f3966b408b7b1eab886d2e30721c72d9db93
Hacker-C <1507559148@qq.com>
2022-07-29 13:26:25 +0800

    💡 Change link

--- questions/305-capitalize/VInput.vue
+++ questions/305-capitalize/VInput.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 // 考察自定义 v-model 修饰符
 // Handling v-model modifiers
-// Vue3 docs: https://vuejs.org/guide/components/events.html#usage-with-v-model
+// Vue3 docs: https://vuejs.org/guide/components/events.html#handling-v-model-modifiers
 const props = defineProps<{
   modelValue: string
   modelModifiers: { default: () => {} }
