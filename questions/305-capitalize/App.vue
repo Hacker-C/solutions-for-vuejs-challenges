3cd0e6b16f7b6fe1a38517000ba118155a117eea
Hacker-C <1507559148@qq.com>
2022-07-29 13:21:13 +0800

    🎉 feat: finish #305

---
+++ questions/305-capitalize/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+import VInput from './VInput.vue'
+const value = ref('')
+</script>
+
+<template>
+  <VInput type="text" v-model.capitalize="value" />
+</template>
---
+++ questions/305-capitalize/VInput.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+// 考察自定义 v-model 修饰符
+// Handling v-model modifiers
+// Vue3 docs: https://vuejs.org/guide/components/events.html#usage-with-v-model
+const props = defineProps<{
+  modelValue: string
+  modelModifiers: { default: () => {} }
+}>()
+const emit = defineEmits(['update:modelValue'])
+function emitValue(e: Event) {
+  let value = (e.target as HTMLInputElement).value
+  if (props.modelModifiers['capitalize']) {
+    value = value.slice(0, 1).toUpperCase() + value.slice(1)
+  }
+  emit('update:modelValue', value)
+}
+</script>
+
+<template>
+  <input type="text" @input="emitValue" :value="modelValue" />
+</template>
