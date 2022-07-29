6aa8f8c42ca5ed1b1d7bbf3b7b3bc200df503822
Hacker-C <1507559148@qq.com>
2022-07-29 10:54:27 +0800

    🎉 feat: Finish #243

---
+++ questions/243-prevent-event-propagation/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const click1 = () => {
+  console.log('click1')
+}
+
+const click2 = () => {
+  console.log('click2')
+}
+</script>
+
+<template>
+  <div @click.stop="click1()"><div @click.stop="click2()">click me</div></div>
+</template>
