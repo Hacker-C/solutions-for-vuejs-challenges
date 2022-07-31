38360d6da520409cf789fb0cfb567969d265e155
Hacker-C <1507559148@qq.com>
2022-07-31 16:20:43 +0800

    🎉 feat: Finish #7

---
+++ questions/7-raw-api/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { reactive, isReactive, toRaw } from 'vue'
+
+const state = { count: 1 }
+const reactiveState = reactive(state)
+
+/**
+ * Modify the code so that we can make the output be true.
+ */
+console.log(toRaw(reactiveState) === state)
+
+/**
+ * Modify the code so that we can make the output be false.
+ */
+const info = { count: 1 }
+const reactiveInfo = reactive(info)
+
+console.log(isReactive(toRaw(reactiveInfo)))
+</script>
+
+<template>
+  <div>
+    <p>
+      {{ reactiveState.count }}
+    </p>
+  </div>
+</template>
---
+++ questions/7-raw-api/index.test.ts
@@ -0,0 +1,15 @@
+import { mount } from '@vue/test-utils'
+import { describe, it, expect, vi } from 'vitest'
+
+import App from './App.vue'
+
+describe('raw API', () => {
+  it('should work', async () => {
+    const result: string[] = []
+    console.log = vi.fn((log: string) => {
+      result.push(log)
+    })
+    mount(App)
+    expect(JSON.stringify(result)).toBe(JSON.stringify([true, false]))
+  })
+})
