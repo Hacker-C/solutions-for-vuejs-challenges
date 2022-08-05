cc127ec44b2febd8c21372bbf1704a884faf37b6
Hacker-C <1507559148@qq.com>
2022-08-05 11:04:44 +0800

    🎉 feat: Finish #17

---
+++ questions/17-useCounter/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+interface UseCounterOptions {
+  min?: number
+  max?: number
+}
+
+/**
+ * Implement the composable function
+ * Make sure the function works correctly
+ */
+function useCounter(initialValue = 0, options: UseCounterOptions = {}) {
+  const count = ref(initialValue)
+  const inc = () => void (count.value < options.max && count.value++)
+  const dec = () => void (count.value > options.min && count.value--)
+  const reset = () => void (count.value = 0)
+  return {
+    count,
+    inc,
+    dec,
+    reset
+  }
+}
+
+const { count, inc, dec, reset } = useCounter(0, { min: 0, max: 10 })
+</script>
+
+<template>
+  <p>Count: {{ count }}</p>
+  <button @click="inc">inc</button>
+  <button @click="dec">dec</button>
+  <button @click="reset">reset</button>
+</template>
