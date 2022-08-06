3a7d830ff76e0c026f4b3913c1736ecd3b60dfdd
Hacker-C <1507559148@qq.com>
2022-08-06 18:35:56 +0800

    ✨ feat: Finish #19
    自定义指令

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './questions/8-effect-scope/App.vue'
+import App from './questions/19-v-focus/App.vue'
 
 createApp(App).mount('#app')
---
+++ questions/19-v-focus/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const state = ref(false)
+
+/**
+ * Implement the custom directive
+ * Make sure the input element focuses/blurs when the 'state' is toggled
+ *
+ */
+
+interface VF {
+  updated(el: HTMLInputElement, obj: { value: boolean }): void
+}
+
+const VFocus: VF = {
+  updated: (el, { value }) => (value ? el.focus() : el.blur())
+}
+
+setInterval(() => {
+  state.value = !state.value
+}, 2000)
+</script>
+
+<template>
+  <input v-focus="state" type="text" />
+</template>
