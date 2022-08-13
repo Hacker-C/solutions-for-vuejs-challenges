e3a5e03057fc8c63f9b7650d4b3ac8e4abaa9161
Hacker-C <1507559148@qq.com>
2022-08-13 10:59:39 +0800

    ✨ feat: Finish #27
    Vuejs style global-css

---
+++ questions/27-global-css/index.test.ts
@@ -0,0 +1,9 @@
+import { describe, it, expect } from 'vitest'
+
+import AppRaw from './App.vue?raw'
+
+describe('DomPortal', () => {
+  it('render to body', () => {
+    expect(AppRaw).toContain(atob('Omdsb2JhbChib2R5KQ=='))
+  })
+})
